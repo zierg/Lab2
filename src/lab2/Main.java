da0133e9f5f9f77c18c9028b03d3f1bc3b0f7ab9
zierg <zierg@yandex.ru>
2013-12-01 19:59:47 +0400

    Simple InetAddress test

--- src/lab2/Main.java
+++ src/lab2/Main.java
@@ -1,9 +1,13 @@
 package lab2;
 
+import java.net.*;
+
 public class Main {
-
-    public static void main(String[] args) {
-
+    public static void main(String[ ] args) throws Exception {
+        InetAddress address = InetAddress.getLocalHost();
+        System.out.println(address);
+        
+        address = InetAddress.getByName("yandex.ru");
+        System.out.println(address);
     }
-
 }
