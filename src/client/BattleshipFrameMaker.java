03d861c8f72c3759e9c441667eb6a617758d3b5e
Ваня <@NEW>
2013-12-05 14:12:42 +0400

    Battlefields protype, doesn't work

--- src/client/BattleshipFrameMaker.java
+++ src/client/BattleshipFrameMaker.java
@@ -3,14 +3,19 @@ package client;
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
+import java.io.File;
 
 class BattleshipFrameMaker {
+    private static final int FIELDS_COUNT = 10;
+    private static JButton[][] playerField;
+    private static JButton[][] enemyField;
+    
     private BattleshipFrameMaker() {}
     
     public static JFrame createFrame() {
         JFrame battleshipFrame = new JFrame("Battleship");
         battleshipFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        battleshipFrame.setSize(400, 400);
+        battleshipFrame.setSize(800, 400);
         battleshipFrame.setResizable(false);
         
         GridBagLayout layout = new GridBagLayout();
@@ -21,23 +26,30 @@ class BattleshipFrameMaker {
         
         constraints.fill = GridBagConstraints.BOTH;
         
-        JButton b1 = new JButton("b1");
-        JButton b2 = new JButton("b2");
 
-        constraints.gridwidth = 1;
+        /*constraints.gridwidth = 1;
         layout.setConstraints(b1, constraints);
         constraints.gridwidth = GridBagConstraints.REMAINDER;
         layout.setConstraints(b2, constraints);
         
-        battleshipFrame.add(b1);
-        battleshipFrame.add(b2);
+        //battleshipFrame.add(b1);
+        //battleshipFrame.add(b2);*/
+        
         
         constraints.weighty = 1;
-        constraints.gridheight = 10;
-        constraints.gridwidth = 10;
-        JPanel battlefield = createBattlefield(false);
+        constraints.gridheight = 5;
+        constraints.gridwidth = 5;
+        constraints.insets = new Insets(0, 0, 0, 5);
+        
+        JPanel battlefield = createBattlefield(true);
         layout.setConstraints(battlefield, constraints);
         battleshipFrame.add(battlefield);
+        
+        
+        constraints.insets = new Insets(0, 5, 0, 0);
+        JPanel battlefield2 = createBattlefield(false);
+        layout.setConstraints(battlefield2, constraints);
+        battleshipFrame.add(battlefield2);
         
         
         return battleshipFrame;
@@ -46,9 +58,15 @@ class BattleshipFrameMaker {
     private static JPanel createBattlefield(boolean enabled) {
         JPanel battlefield = new JPanel(new GridLayout(10,10));
         
+        
+        ImageIcon icon = new ImageIcon("data" + File.separator + "1.png");
+        
+        Color color = enabled ? Color.white : new Color(0xF0F0F0);
+        
         for (int i = 0; i < 10; i++) {
             for (int j = 0; j < 10; j++) {
-                JButton newButton = new JButton(i + ", " + j);
+                JButton newButton = new JButton();
+                newButton.setBackground(color);
                 newButton.setEnabled(enabled);
                 battlefield.add(newButton);
             }
