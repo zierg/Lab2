db53b3e480acabfe9c8f83f935d1d3e33917bc7b
zierg <zierg@yandex.ru>
2013-12-09 19:31:48 +0400

    Painting ship (better than before)

--- src/client/battleship/Ship.java
+++ src/client/battleship/Ship.java
@@ -9,39 +9,40 @@ import javax.swing.JToggleButton;
 
 class Ship extends JToggleButton {
     private static final int BLOCK_INSET = 1;
+    
+    private static class ShipIcon implements Icon {
+        private final int shipSize;
+        private final int sideSize;
+
+        private final int inset;
+
+        public ShipIcon(int shipSize, int sideSize, int inset) {
+            this.sideSize = sideSize;
+            this.shipSize = (sideSize + inset)*shipSize;
+            this.inset = inset;
+        }
+
+        @Override
+        public int getIconHeight() {
+            return sideSize;
+        }
+
+        @Override
+        public int getIconWidth() {
+            return shipSize;
+        }
+
+        @Override
+        public void paintIcon(Component c, Graphics g, int x, int y) {
+            for (int i = 0; i < shipSize; i++) {
+                g.setColor(Color.DARK_GRAY);
+                g.fillRect((sideSize*i) + inset*i, 0, sideSize, sideSize);
+            }
+        }
+    }
+    
     public Ship(int shipSize, int sideSize) {
         super(new ShipIcon(shipSize, sideSize, BLOCK_INSET));
         setPreferredSize(new Dimension(sideSize*shipSize + BLOCK_INSET*shipSize-BLOCK_INSET, sideSize));
     }  
-}
-
-class ShipIcon implements Icon {
-    private final int shipSize;
-    private final int sideSize;
-    
-    private final int inset;
-
-    public ShipIcon(int shipSize, int sideSize, int inset) {
-        this.sideSize = sideSize;
-        this.shipSize = (sideSize + inset)*shipSize;
-        this.inset = inset;
-    }
-
-    @Override
-    public int getIconHeight() {
-        return sideSize;
-    }
-
-    @Override
-    public int getIconWidth() {
-        return shipSize;
-    }
-
-    @Override
-    public void paintIcon(Component c, Graphics g, int x, int y) {
-        for (int i = 0; i < shipSize; i++) {
-            g.setColor(Color.DARK_GRAY);
-            g.fillRect((sideSize*i) + inset*i, 0, sideSize, sideSize);
-        }
-    }
 }
