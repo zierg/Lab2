78559083713e19b313da6361de26b5ae70166e8b
zierg <zierg@yandex.ru>
2013-12-09 20:21:03 +0400

    OK, let it be

--- src/client/battleship/BrowseShipPanel.java
+++ src/client/battleship/BrowseShipPanel.java
@@ -9,7 +9,13 @@ public class BrowseShipPanel extends JPanel {
         super();
         setLayout(new FlowLayout());
         add (new Ship(1, 30));
+        add (new Ship(1, 30));
+        add (new Ship(1, 30));
+        add (new Ship(1, 30));
         add (new Ship(2, 30));
+        add (new Ship(2, 30));
+        add (new Ship(2, 30));
+        add (new Ship(3, 30));
         add (new Ship(3, 30));
         add (new Ship(4, 30));
         /*add(new JButton());
