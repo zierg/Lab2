3f196ffaa0b2bb938f1e8d28574a3a15719977c5
zierg <zierg@yandex.ru>
2013-12-09 19:30:37 +0400

    Painting ship

--- src/client/battleship/Ship.java
+++ src/client/battleship/Ship.java
@@ -1,13 +1,47 @@
 package client.battleship;
 
+import java.awt.Color;
+import java.awt.Component;
 import java.awt.Dimension;
-import javax.swing.JButton;
+import java.awt.Graphics;
+import javax.swing.Icon;
+import javax.swing.JToggleButton;
 
-public class Ship extends JButton {
-    private static final int BLOCK_INSET = 2;
-    
+class Ship extends JToggleButton {
+    private static final int BLOCK_INSET = 1;
     public Ship(int shipSize, int sideSize) {
-        super();
-        setPreferredSize(new Dimension(sideSize, sideSize*shipSize));
+        super(new ShipIcon(shipSize, sideSize, BLOCK_INSET));
+        setPreferredSize(new Dimension(sideSize*shipSize + BLOCK_INSET*shipSize-BLOCK_INSET, sideSize));
+    }  
+}
+
+class ShipIcon implements Icon {
+    private final int shipSize;
+    private final int sideSize;
+    
+    private final int inset;
+
+    public ShipIcon(int shipSize, int sideSize, int inset) {
+        this.sideSize = sideSize;
+        this.shipSize = (sideSize + inset)*shipSize;
+        this.inset = inset;
+    }
+
+    @Override
+    public int getIconHeight() {
+        return sideSize;
+    }
+
+    @Override
+    public int getIconWidth() {
+        return shipSize;
+    }
+
+    @Override
+    public void paintIcon(Component c, Graphics g, int x, int y) {
+        for (int i = 0; i < shipSize; i++) {
+            g.setColor(Color.DARK_GRAY);
+            g.fillRect((sideSize*i) + inset*i, 0, sideSize, sideSize);
+        }
     }
 }
