802c83833d66551158d581fba562a7d512fcb4aa
zierg <zierg@yandex.ru>
2013-12-09 20:13:29 +0400

    Add ship rotation

--- src/client/battleship/Ship.java
+++ src/client/battleship/Ship.java
@@ -8,41 +8,100 @@ import javax.swing.Icon;
 import javax.swing.JToggleButton;
 
 class Ship extends JToggleButton {
-    private static final int BLOCK_INSET = 1;
-    
     private static class ShipIcon implements Icon {
-        private final int shipSize;
         private final int sideSize;
+        private final int shipSize;
+        private final Color color;
 
-        private final int inset;
-
-        public ShipIcon(int shipSize, int sideSize, int inset) {
+        private final int rotation;
+        
+        public ShipIcon(int shipSize, int sideSize, int rotation, Color color) {
             this.sideSize = sideSize;
-            this.shipSize = (sideSize + inset)*shipSize;
-            this.inset = inset;
+            this.shipSize = shipSize;
+            this.color = color;
+            this.rotation = rotation;
         }
 
         @Override
         public int getIconHeight() {
-            return sideSize;
+            if (this.rotation == SHIP_HORIZONTAL) {
+                return sideSize;
+            } else {
+                return (sideSize + BLOCK_INSET)*shipSize;
+            }
         }
 
         @Override
         public int getIconWidth() {
-            return shipSize;
+            if (this.rotation == SHIP_HORIZONTAL) {
+                return (sideSize + BLOCK_INSET)*shipSize;
+            } else {
+                return sideSize;
+            }
         }
 
         @Override
         public void paintIcon(Component c, Graphics g, int x, int y) {
-            for (int i = 0; i < shipSize; i++) {
-                g.setColor(Color.DARK_GRAY);
-                g.fillRect((sideSize*i) + inset*i, 0, sideSize, sideSize);
+            g.setColor(color);
+            if (this.rotation == SHIP_HORIZONTAL) {
+                for (int i = 0; i < shipSize; i++) {
+                    g.fillRect((sideSize*i) + BLOCK_INSET*i, 0, sideSize, sideSize);
+                }
+            } else {
+                for (int i = 0; i < shipSize; i++) {
+                    g.fillRect(0, (sideSize*i) + BLOCK_INSET*i, sideSize, sideSize);
+                }
             }
         }
     }
+    public static final int SHIP_HORIZONTAL = 1;
+    public static final int SHIP_VERTICAL = 0;
+    
+    private static final int BLOCK_INSET = 1;
+    
+    private int rotation;
+    
+    private final ShipIcon horizontalUnselectedIcon;
+    private final ShipIcon verticalUnselectedIcon;
+    private final ShipIcon horizontalSelectedIcon;
+    private final ShipIcon verticalSelectedIcon;
+    
+    private final int shipSize;
+    private final int sideSize;
     
     public Ship(int shipSize, int sideSize) {
-        super(new ShipIcon(shipSize, sideSize, BLOCK_INSET));
-        setPreferredSize(new Dimension(sideSize*shipSize + BLOCK_INSET*shipSize-BLOCK_INSET, sideSize));
-    }  
+        super();
+        this.shipSize = shipSize;
+        this.sideSize = sideSize;
+        horizontalUnselectedIcon = new ShipIcon(shipSize, sideSize, SHIP_HORIZONTAL, Color.darkGray);
+        verticalUnselectedIcon = new ShipIcon(shipSize, sideSize, SHIP_VERTICAL, Color.darkGray);
+        horizontalSelectedIcon = new ShipIcon(shipSize, sideSize, SHIP_HORIZONTAL, Color.lightGray);
+        verticalSelectedIcon = new ShipIcon(shipSize, sideSize, SHIP_VERTICAL, Color.lightGray);
+        setRotation(SHIP_VERTICAL);
+    }
+    
+    public int getRotation() {
+        return rotation;
+    }
+    
+    public void setRotation(int rotation) {
+        this.rotation = rotation;
+        switch (rotation) {
+            case SHIP_HORIZONTAL: {
+                setPreferredSize(new Dimension(sideSize*shipSize + BLOCK_INSET*shipSize-BLOCK_INSET, sideSize));
+                setIcon(horizontalUnselectedIcon);
+                setSelectedIcon(horizontalSelectedIcon);
+                break;
+            }
+            case SHIP_VERTICAL: {
+                setPreferredSize(new Dimension(sideSize, sideSize*shipSize + BLOCK_INSET*shipSize-BLOCK_INSET));
+                setIcon(verticalUnselectedIcon);
+                setSelectedIcon(verticalSelectedIcon);
+                break;
+            }
+            default: {
+                throw new UncorrectModeException();
+            }
+        }
+    }
 }
