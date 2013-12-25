9cc60a352ee6298117781026a12a23179009a348
zierg <zierg@yandex.ru>
2013-12-25 17:38:50 +0400

    Add listeners list to BrowseShipPanel

--- src/client/battleship/BrowseShipPanel.java
+++ src/client/battleship/BrowseShipPanel.java
@@ -1,8 +1,21 @@
 package client.battleship;
 
+import client.battleship.events.BrowseShipPanelActionListener;
+import java.util.LinkedList;
+import java.util.List;
 import javax.swing.JPanel;
 
 abstract class BrowseShipPanel extends JPanel {
+    protected List<BrowseShipPanelActionListener> listeners = new LinkedList<>();
+    
+    public BrowseShipPanel() {
+        super();
+    }
+
+    public void addBattlefieldActionListener(BrowseShipPanelActionListener listener) {
+        listeners.add(listener);
+    }
+    
     public abstract Ship getSelectedShip();
     public abstract void deleteSelectedShip();
 }
