fd2f3ce6ad0dafe3c9866f2752d6ec12f9368b08
zierg <zierg@yandex.ru>
2013-12-25 17:31:44 +0400

    Add events for BrowseShipPanel

---
+++ src/client/battleship/events/BrowseShipPanelActionEvent.java
@@ -0,0 +1,20 @@
+package client.battleship.events;
+
+import java.util.EventObject;
+
+public class BrowseShipPanelActionEvent extends EventObject {
+    private String message;
+    
+    public BrowseShipPanelActionEvent(Object source) {
+        super(source);
+    }
+    
+    public BrowseShipPanelActionEvent(Object source, String message) {
+        super(source);
+        this.message = message;
+    }
+    
+    public String getMessage() {
+        return message;
+    }
+}
---
+++ src/client/battleship/events/BrowseShipPanelActionListener.java
@@ -0,0 +1,5 @@
+package client.battleship.events;
+
+public interface BrowseShipPanelActionListener {
+    public void actionPerformed(BrowseShipPanelActionEvent e);
+}
