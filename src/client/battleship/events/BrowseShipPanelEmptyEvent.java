9825b345de0a4411fd6f96de434b0ae508fab433
zierg <zierg@yandex.ru>
2013-12-26 12:56:41 +0400

    Add empty event and listener

---
+++ src/client/battleship/events/BrowseShipPanelEmptyEvent.java
@@ -0,0 +1,20 @@
+package client.battleship.events;
+
+import java.util.EventObject;
+
+public class BrowseShipPanelEmptyEvent extends EventObject {
+    private String message;
+    
+    public BrowseShipPanelEmptyEvent(Object source) {
+        super(source);
+    }
+    
+    public BrowseShipPanelEmptyEvent(Object source, String message) {
+        super(source);
+        this.message = message;
+    }
+    
+    public String getMessage() {
+        return message;
+    }
+}
---
+++ src/client/battleship/events/BrowseShipPanelEmptyListener.java
@@ -0,0 +1,5 @@
+package client.battleship.events;
+
+public interface BrowseShipPanelEmptyListener {
+    public void panelIsEmpty(BrowseShipPanelEmptyEvent e);
+}
