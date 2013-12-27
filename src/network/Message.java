320a708d2bf6b3aed2238e984643a23693faf556
zierg <zierg@yandex.ru>
2013-12-27 20:10:18 +0400

    Add some methods and fields to Message

--- src/network/Message.java
+++ src/network/Message.java
@@ -5,9 +5,35 @@ import java.io.Serializable;
 public class Message implements Serializable {
     public static final int TURN = 0;
     
-    private int type;
+    private static final int[] TYPES = {
+        TURN
+    };
     
-    public Message() {
-        
+    private int type;
+    private Serializable[] attributes;
+    
+    public Message(int type, Serializable ... attributes) throws UncorrectMessageTypeException {
+        if (!isTypeCorrect(type)) {
+            throw new UncorrectMessageTypeException();
+        }
+        this.type = type;
+        this.attributes = attributes;
+    }
+    
+    public int getType() {
+        return type;
+    }
+    
+    public Serializable[] getAttributes() {
+        return attributes;
+    }
+    
+    private boolean isTypeCorrect(int type) {
+        for (int currentType:TYPES) {
+            if (type == currentType) {
+                return true;
+            }
+        }
+        return false;
     }
 }
