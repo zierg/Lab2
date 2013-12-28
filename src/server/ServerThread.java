7df7f533bbf7fa35388fd93a175417d2259dfba5
zierg <zierg@yandex.ru>
2013-12-28 15:11:52 +0400

    Add createUser method to ServerThread

--- src/server/ServerThread.java
+++ src/server/ServerThread.java
@@ -28,25 +28,25 @@ class ServerThread extends Thread {
             sout = clientSocket.getOutputStream();
             output = new ObjectOutputStream(sout);
             input = new ObjectInputStream(sin);
-            
-            
-            Message authMessage = (Message) input.readObject();
-            String userName = (String) authMessage.getAttributes()[0];
-            
-            user = new User(userName, clientSocket);
-            Server.addUser(user);
-            System.out.println(user.getName());
+            if ( !createUser() ) {
+                return;
+            }                       
         } catch (IOException ex) {
-            try {
-                System.out.println("User " + user + " is offline.");
-                Server.removeUser(user);
-            } catch (NullPointerException ex1) {System.out.println("asd");}
-        } catch (ClassNotFoundException ex) {
-            //Logger.getLogger(ServerThread.class.getName()).log(Level.SEVERE, null, ex);
+            System.out.println("some error occured");   // Добавить логгер
         }
     }
     
     private boolean createUser() {
-        
+        try {
+            Message authMessage = (Message) input.readObject();
+            String userName = (String) authMessage.getAttributes()[0];
+
+            user = new User(userName, clientSocket);
+            Server.addUser(user);
+            System.out.println(user.getName());
+            return true;
+        } catch (IOException | ClassNotFoundException ex) {
+             return false;
+        }
     }
 }
