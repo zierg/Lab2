c627024d216c25c084f343bcb4a2fe0f248cbc4e
zierg <zierg@yandex.ru>
2013-12-28 20:28:11 +0400

    Make ClientMessenger abstract

---
+++ src/client/NetworkClientMessenger.java
@@ -0,0 +1,26 @@
+package client;
+
+import java.io.IOException;
+import network.Message;
+import network.Messenger;
+
+public class NetworkClientMessenger extends ClientMessenger {
+    
+    private final Messenger messenger;
+    
+    public NetworkClientMessenger(Messenger messenger) {
+        this.messenger = messenger;
+    }
+    
+    public boolean connect(String userName) {
+        try {
+            Message authMessage = new Message(Message.AUTHORIZATION, userName);
+            messenger.sendMessage(authMessage);
+        } catch (IOException ex) {
+            //System.out.println("FAIL (nothing epic..)");
+            return false;
+        }
+        //System.out.println("Successfully conected =)");
+        return true;
+    }
+}
