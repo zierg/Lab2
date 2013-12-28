38b3150469910950ba0fa1359e73e916f1a256f1
zierg <zierg@yandex.ru>
2013-12-28 21:13:41 +0400

    Delete unused classes

--- src/client/ClientMessenger.java
+++
@@ -1,5 +0,0 @@
-package client;
-
-public abstract class ClientMessenger {
-    public abstract boolean connect(String userName);
-}
--- src/network/Messenger.java
+++
@@ -1,8 +0,0 @@
-package network;
-
-import java.io.IOException;
-
-public interface Messenger {
-    public Message getMessage() throws IOException;
-    public void sendMessage(Message message) throws IOException;
-}
