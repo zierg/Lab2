b0dc4f1cc5074fba81713218b50cf4b4b42b1c2a
zierg <zierg@yandex.ru>
2013-12-28 12:37:00 +0400

    Add some methods to User

--- src/network/User.java
+++ src/network/User.java
@@ -1,5 +1,26 @@
 package network;
 
+import java.net.Socket;
+
 public class User {
+    private final String userName;
+    private final Socket socket;
     
+    public User(String userName, Socket socket) {
+        this.userName = userName;
+        this.socket = socket;
+    }
+    
+    public String getName() {
+        return userName;
+    }
+    
+    public Socket getSocket() {
+        return socket;
+    }
+    
+    @Override
+    public String toString() {
+        return userName;
+    }
 }
