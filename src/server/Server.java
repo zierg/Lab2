6fc529bb8c85dc789a3cd76e0f47785e5a5e3527
zierg <zierg@yandex.ru>
2013-12-28 13:38:40 +0400

    Add some methods to Server

--- src/server/Server.java
+++ src/server/Server.java
@@ -1,5 +1,42 @@
 package server;
 
-public class Server {
+import network.*;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Vector;
+
+
+public class Server {
+    public static final int PORT = 12345;
+    private static Vector<User> usersList = new Vector<>();
+    
+    public static void main(String[] args) {
+        try {
+            ServerSocket serverSocket = new ServerSocket(PORT);
+            System.out.println("Server is running.");
+            while (true) {
+                Socket client = serverSocket.accept();  // заставляем сервер ждать подключений и выводим сообщение,
+                System.out.println("Got a client.");    // когда кто-то связался с сервером
+                User newUser = new User("Pit", client);
+                addUser(newUser);
+                new ServerThread(newUser);
+            }
+        } catch (IOException ex) {
+            
+        }
+    }
+    
+    public static void addUser(User user) {
+        usersList.add(user);
+    }
+    
+    public static void removeUser(User user) {
+        usersList.remove(user);
+    }
+    
+    public synchronized static Vector<User> getUsers() {
+        return usersList;
+    }
 }
