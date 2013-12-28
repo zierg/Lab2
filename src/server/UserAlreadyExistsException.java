08d9ab8a0c5376a80a8e2c6bd20bbbfeaf97395c
zierg <zierg@yandex.ru>
2013-12-28 13:57:42 +0400

    Add UserAlreadyExistsException

---
+++ src/server/UserAlreadyExistsException.java
@@ -0,0 +1,19 @@
+package server;
+
+public class UserAlreadyExistsException extends Exception {
+    public UserAlreadyExistsException() {
+        super();
+    }
+    
+    public UserAlreadyExistsException(String message) {
+        super(message);
+    }
+    
+    public UserAlreadyExistsException(String message, Throwable cause) {
+        super(message, cause);
+    }
+    
+    public UserAlreadyExistsException(Throwable cause) {
+        super(cause);
+    }
+}
